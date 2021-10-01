90fc170267ea025601f0e399fc3a0fbaf1013e61
jhhg0 <jhherrera@ccss.sa.cr>
2021-10-01 14:40:20 -0500

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 const url = require('url');
 const path = require('path');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
+  });
+}
+
 let mainWindow;
+let newProductWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
@@ -14,4 +21,79 @@ app.on('ready', () => {
       slashes: true,
     })
   );
+
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
 });
+
+function createNewProductWindow() {
+  newProductWindow = new BrowserWindow({
+    width: 400,
+    height: 330,
+    title: 'Add A New Product',
+  });
+  // newProductWindow.setMenu(null);
+  newProductWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'views/new-product.html'),
+      protocol: 'file',
+      slashes: true,
+    })
+  );
+  newProductWindow.on('closed', () => {
+    newProductWindow = null;
+  });
+}
+
+const templateMenu = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Product',
+        accelerator: process.platform === 'darwin' ? 'command+Q' : 'Ctrl+N',
+        click() {
+          createNewProductWindow();
+        },
+      },
+      {
+        label: 'Remove All Products',
+      },
+      {
+        label: 'Exit',
+        accelerator: process.platform === 'darwin' ? 'command+Q' : 'Ctrl+N',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+if (process.platform === 'darwin') {
+  templateMenu.unshift({
+    label: app.getName(),
+  });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  templateMenu.push({
+    label: 'DevTools',
+    submenu: [
+      {
+        label: 'Show/Hide Dev Tools',
+        accelerator: process.platform === 'darwin' ? 'command+D' : 'Ctrl+D',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: 'reload',
+      },
+    ],
+  });
+}
